bec7c38eb78616bb7f4f24b4ef4a0a5524f6a095
Roman Ortega Mondaca <romtega88@gmail.com>
2023-09-30 15:43:30 +0200

    fixed scroll bar-table

--- js/db.js
+++ js/db.js
@@ -1,1 +1,83 @@
-console.log("working");
+"use strict";
+
+const student = [
+  {
+    name: "Cesar",
+    lastName: "Gutierrez Franco",
+    age: 22,
+    genre: "Masculino",
+    id: "#479c1c",
+    courses: 4,
+    score: 10,
+    email: "ceguf02@hotmail.com",
+  },
+  {
+    name: "Pedro",
+    lastName: "Contreras Martinez",
+    age: 30,
+    genre: "Masculino",
+    id: "#123c56",
+    courses: 3,
+    score: 9.3,
+    email: "montenegro.catarina@gmail.com",
+  },
+  {
+    name: "Patricia",
+    lastName: "Guzman Aguilar",
+    age: 25,
+    genre: "Femenino",
+    id: "#90fc6d",
+    courses: 2,
+    score: 8.2,
+    email: "lvalentin@valdez.net.br",
+  },
+  {
+    name: "Leticia",
+    lastName: " Lopez Lopez",
+    age: 28,
+    genre: "Femenino",
+    id: "#d0d584",
+    courses: 4,
+    score: 8.6,
+    email: "thales.vega@duarte.br",
+  },
+  {
+    name: "Juan Manuel",
+    lastName: "Gutierrez Perez",
+    age: 30,
+    genre: "Otro",
+    id: "#009b75",
+    courses: 1,
+    score: 9.8,
+    email: "ricardo64@gmail.com",
+  },
+  {
+    name: "Melanie",
+    lastName: " Garza Fuentes",
+    age: 25,
+    genre: "",
+    id: "#72be03",
+    courses: 4,
+    score: 9.5,
+    email: "hleon@carrara.com",
+  },
+];
+
+const teachers = [
+  {
+    fullName: "Pedro Salazar",
+    course: "Javascript",
+  },
+  {
+    fullName: "Lucía Serratos",
+    course: "Python",
+  },
+  {
+    fullName: "Santiago González",
+    course: "Ruby on Rails",
+  },
+  {
+    fullName: "José Agular",
+    course: "Swift",
+  },
+];
