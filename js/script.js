f1e34945a2d46129543a68aeea043405ff2e92e3
Roman Ortega Mondaca <romtega88@gmail.com>
2023-10-03 02:45:47 +0200

    added img Input to form

--- js/script.js
+++ js/script.js
@@ -36,6 +36,8 @@ const studentsList = document.querySelector("#students-list");
 
 for (let i = 0; i < students.length; i++) {
   const row = document.createElement("tr");
+  const rowColor = i % 2 === 0 ? "row-gray" : "row-white";
+  row.classList.add(rowColor);
 
   const imgCell = document.createElement("td");
   const img = document.createElement("img");
@@ -47,10 +49,7 @@ for (let i = 0; i < students.length; i++) {
   const scoreCell = document.createElement("td");
   const deleteCell = document.createElement("td");
 
-  const rowColor = i % 2 === 0 ? "row-gray" : "row-white";
-  row.classList.add(rowColor);
-
-  img.src = students[i].imgPath;
+  img.src = students[i].imgPath || "";
   img.alt = `${students[i].name} ${students[i].lastName}`;
   imgCell.appendChild(img);
   nameCell.textContent = students[i].name;
@@ -102,53 +101,84 @@ searchBar.addEventListener("input", function () {
 
 /************** NEW STUDENT FORM ***************/
 
-// const studentForm = document.querySelector("#form-new");
-// const nameInput = studentForm["name"];
-// const lastNameInput = studentForm["last-name"];
-// const ageInput = studentForm["age"];
-// const genreInput = studentForm["genre"];
-// const emailInput = studentForm["email"];
-// const idInput = studentForm["id"];
-// const coursesInput = studentForm["courses"];
-// const scoreInput = studentForm["score"];
+class Student {
+  constructor(name, lastName, age, genre, email, id, courses, score, imgPath) {
+    this.name = name;
+    this.lastName = lastName;
+    this.age = age;
+    this.genre = genre;
+    this.id = id;
+    this.courses = courses;
+    this.score = score;
+    this.email = email;
+    this.imgPath = imgPath || "";
+  }
+}
 
-// studentForm.addEventListener("submit", function (e) {
-//   e.preventDefault();
+const studentForm = document.querySelector("#form-new");
 
-// //   const student = new Student(
-// //     nameInput,
-// //     lastNameInput,
-// //     ageInput,
-// //     genreInput,
-// //     emailInput,
-// //     idInput,
-// //     coursesInput,
-// //     scoreInput
-// //   );
+studentForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const nameInput = studentForm["name"].value;
+  const lastNameInput = studentForm["last-name"].value;
+  const ageInput = studentForm["age"].value;
+  const genreInput = studentForm["genre"].value;
+  const idInput = studentForm["id"].value;
+  const coursesInput = studentForm["courses"].value;
+  const scoreInput = studentForm["score"].value;
+  const emailInput = studentForm["email"].value;
+  const imgPathInput = studentForm["imgPath"].value;
 
-// //   function addStudent(student) {
-// //     document.createElement("tr").innerHTML = `
-// //     <td>${student.name}</td>
-// //     <td>${student.lastName}</td>
-// //     <td>${student.age}</td>
-// //     <td>${student.courses}</td>
-// //     <td>${student.score}</td>
-// //     `;
-// //     studentsList.appendChild(document.createElement("tr"));
-// //   }
-// // });
+  const newStudent = new Student(
+    nameInput,
+    lastNameInput,
+    ageInput,
+    genreInput,
+    emailInput,
+    idInput,
+    coursesInput,
+    scoreInput,
+    imgPathInput
+  );
 
-/************** STUDENTS TABLE ***************/
+  students.push(newStudent);
 
-// class Student {
-//   constructor(name, lastName, age, genre, email, id, courses, score) {
-//     this.name = name;
-//     this.lastName = lastName;
-//     this.age = age;
-//     this.genre = genre;
-//     this.email = email;
-//     this.id = id;
-//     this.courses = courses;
-//     this.score = score;
-//   }
-// }
+  const row = document.createElement("tr");
+  const rowColor = students.length % 2 === 0 ? "row-gray" : "row-white";
+  row.classList.add(rowColor);
+
+  const imgCell = document.createElement("td");
+  const img = document.createElement("img");
+  const nameCell = document.createElement("td");
+  const lastNameCell = document.createElement("td");
+  const ageCell = document.createElement("td");
+  const idCell = document.createElement("td");
+  const coursesCell = document.createElement("td");
+  const scoreCell = document.createElement("td");
+  const deleteCell = document.createElement("td");
+
+  img.src = newStudent.imgPath || "";
+  img.alt = `${newStudent.name} ${newStudent.lastName}`;
+  imgCell.appendChild(img);
+  nameCell.textContent = newStudent.name;
+  lastNameCell.textContent = newStudent.lastName;
+  ageCell.textContent = newStudent.age;
+  idCell.textContent = newStudent.id;
+  coursesCell.textContent = newStudent.courses;
+  scoreCell.textContent = newStudent.score;
+  deleteCell.innerHTML = `<ion-icon class="delete-icon" name="trash"></ion-icon
+  >`;
+
+  row.appendChild(imgCell);
+  row.appendChild(nameCell);
+  row.appendChild(lastNameCell);
+  row.appendChild(ageCell);
+  row.appendChild(idCell);
+  row.appendChild(coursesCell);
+  row.appendChild(scoreCell);
+  row.appendChild(deleteCell);
+
+  studentsList.appendChild(row);
+
+  closeForm();
+});
